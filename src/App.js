546e15fbec82e7b468c723a48a940f34da14b819
Nizam <nizam.salim7@gmail.com>
2021-09-16 19:13:31 +0530

    router test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import Navbar from "./Components/Navbar/Navbar";
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
 import Banner from "./Components/Banner/Banner";
 import RowPoster from "./Components/RowPoster/RowPoster";
-import {originals,action,horror,comedy,romance} from './urls';
-
-
+import { originals, action, horror, comedy, romance } from "./urls";
+import { Route } from "react-router";
+import Profile from "./Components/Profile";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Banner/>
-      <RowPoster url={originals} title='Netflix Originals' />
-      <RowPoster url={action} title='Action' isSmall />
-      <RowPoster url={horror} title='Horror' isSmall />
-      <RowPoster url={comedy} title='Comedy' isSmall />
-      <RowPoster url={romance} title='Romance' isSmall />
-      
+      <Route exact path="/">
+        <Banner />
+        <RowPoster url={originals} title="Netflix Originals" />
+        <RowPoster url={action} title="Action" isSmall />
+        <RowPoster url={horror} title="Horror" isSmall />
+        <RowPoster url={comedy} title="Comedy" isSmall />
+        <RowPoster url={romance} title="Romance" isSmall />
+        <p style={{ color: "#6b6b6b" }}>Designed by Nizam Salim</p>
+      </Route>
+      <Route exact path="/profile">
+        <Profile />
+      </Route>
     </div>
   );
 }
---
+++ src/Components/Profile.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <div>
+      <p style={{ color: "#fff" }}>PROFILE SCREEN</p>
+    </div>
+  );
+}
+
+export default Profile;
